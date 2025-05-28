de935793a69813285844731a42001426edfd96b1
Rodrigo Reyes <rodrigo.m.reyes.v@gmail.com>
2025-05-27 19:03:35 -0600

    first version of server class

--- src/appserver/server.h
+++ src/appserver/server.h
@@ -4,22 +4,28 @@
 #include <asio.hpp>
 #include <iostream>
 #include <memory>
+#include <atomic>
 #include <csignal>
 #include <nlohmann/json.hpp>
 
-using asio::ip::tcp;
-asio::io_context io;
-std::atomic<int> connection_counter{0};
-extern std::atomic<int> connection_counter;
+#include "../common/common.h"
 
+using asio::ip::tcp;
 using json = nlohmann::json;
 
-void handle_signal(const asio::error_code& error, int signal_number);
-void handle_client(std::shared_ptr<tcp::socket> socket, int client_id);
-void on_read(std::shared_ptr<tcp::socket> socket, std::shared_ptr<asio::streambuf> buffer,
-             const asio::error_code& ec, std::size_t length, int client_id);
-void on_write(std::shared_ptr<tcp::socket> socket, const asio::error_code& error_code, std::size_t length, int client_id);
-void on_accept(std::shared_ptr<tcp::socket> socket, tcp::acceptor& acceptor, const asio::error_code& error_code);
-void start_accept(tcp::acceptor& acceptor);
+class Server {
+public:
+    Server(asio::io_context& io, unsigned short port) : io_(io), acceptor_(io, tcp::endpoint(tcp::v4(), port)), connection_counter_(0) {}
+    void start() { start_accept(); }
+
+private:
+
+    void start_accept();
+    void handle_client(std::shared_ptr<tcp::socket> socket, int client_id);
+    
+    asio::io_context& io_;
+    tcp::acceptor acceptor_;
+    std::atomic<int> connection_counter_;
+};
 
 #endif // SERVER_H
